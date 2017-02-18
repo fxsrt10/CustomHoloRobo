da9eade7f19a635c26846983fb54584eb66c6917
fxsrt10 <fxsrt10@gmail.com>
2017-02-17 22:44:01 -0600

    Added project

---
+++ Assets/Scripts/PlayerRobo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRobo : MonoBehaviour {
+
+    public GameObject body { get; set; }
+    public GameObject rightArmWeaponAssault { get; set; }
+    public GameObject leftArmWeaponBomb { get; set; }
+    public GameObject podWeapon { get; set; }
+    public GameObject legs { get; set; }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+
+    public Stats stats;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+public class Stats
+{
+    public int attack { get; set; }
+    public int homing { get; set; }
+    public int speed { get; set; }
+    public int down { get; set; }
+    public int reload { get; set; }
+    public int size { get; set; }
+    public int time { get; set; }
+
+}
